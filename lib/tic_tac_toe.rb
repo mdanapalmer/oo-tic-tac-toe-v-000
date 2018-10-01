6ffdb921828d7cb5e2f0c8bfc2161c1701ace5d4
Dana Palmer <roiai4@hotmail.com>
2018-10-01 03:25:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,5 +40,11 @@ class TicTacToe
 def position_taken?(index)
   @board[index] != " "
 end
+#If the board index is not equal to " " then the position is taken
+
+def valid_move?(index)
+    !position_taken? && index.between?(0,8)
+end
+   
 
 end
